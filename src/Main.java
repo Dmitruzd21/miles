6be89c21961b4578327468bddebc0a9962937391
Korotkov Dmitry <2110821@mail.ru>
2021-07-16 23:13:29 +0300

    Код ПО. Версия 1.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketprice = 1998;
+        int onebonusmile = 20;
+        int numberofbonusmiles = ticketprice / onebonusmile;
+        int totalamountofbonusmiles = numberofbonusmiles;
+        System.out.println(totalamountofbonusmiles);
+    }
+}
